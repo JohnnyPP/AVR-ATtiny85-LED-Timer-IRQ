6031e6b889f7f2f289d4458642525d2657a47c46
JohnnyPP <johnnnyppp@gmail.com>
2015-10-09 22:52:34 +0200

    0.5 second period on ATtiny85 with default internal 1 MHz clock No prescaler was used with total overflows of 1953 and 32 additional ticks

--- main.c
+++ main.c
@@ -11,18 +11,30 @@
 
 #define TCCR0B (*(volatile uint8_t *)((0x33) + 0x20))
 
-// To obtain 0.5 second period on ATtiny85 with default internal 1 Mhz clock
-// The 256 prescaler was used with total overflows of 7 and 161 additional ticks
+// Prescaler on page 80
+/* 	CS02 	CS01 	CS00 	Description
+	0 		0 		0 		No clock source (Timer/Counter stopped)
+	0 		0 		1 		clk I/O /(No prescaling)
+	0		1 		0 		clk I/O /8 (From prescaler)
+	0 		1 		1 		clk I/O /64 (From prescaler)
+	1 		0 		0 		clk I/O /256 (From prescaler)
+	1 		0 		1 		clk I/O /1024 (From prescaler)
+	1 		1 		0 		External clock source on T0 pin. Clock on falling edge.
+	1 		1		1 		External clock source on T0 pin. Clock on rising edge.*/
+
+// To obtain 0.5 second period on ATtiny85 with default internal 1 MHz clock
+// No prescaler was used with total overflows of 1953 and 32 additional ticks
 // For calculations use: http://eleccelerator.com/avr-timer-calculator/
+// Using prescaler decreases counter resolution.
 
 // global variable to count the number of overflows
-volatile uint8_t overflowCount;
+volatile uint16_t overflowCount;
 
 // initialize timer, interrupt and variable
 void timer0Initialization()
 {
     // set up timer with prescaler = 256
-	TCCR0B |= (1 << CS02);
+	TCCR0B |= (1 << CS00);
 
     // initialize counter
     TCNT0 = 0;
@@ -56,9 +68,9 @@ int main(void)
     // loop forever
     while(1)
     {
-        if (overflowCount >= 7)
+        if (overflowCount >= 1953)
         {
-            if (TCNT0 >= 161)
+            if (TCNT0 >= 32)
             {
                 PORTB ^= (1 << PINB3);    	// toggles the LED on PC0
                 TCNT0 = 0;            		// reset timer
